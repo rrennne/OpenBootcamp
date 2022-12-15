69be0745f44dfc94900b8b20a2bb2ecda618c626
rrennne <reneconquezo@gmail.com>
2022-12-14 21:16:23 -0600

    Este es mi primer commit

---
+++ Ejercicio#2p/Program.cs
@@ -0,0 +1,11 @@
+﻿//EJERCICIO TEMA #4
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // int numerolf;
+        // int numeroWhile;
+        System.Console.WriteLine("Hola");
+    }
+}
